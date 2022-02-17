f906916ac43d65926537702e5023c8ad4f6fa3ba
jrbart <jorabart@gmail.com>
2022-02-17 13:58:51 -0600

    Configured to run on XQuartz

--- config.mk
+++ config.mk
@@ -7,8 +7,10 @@ VERSION = 6.3
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+#X11INC = /usr/X11R6/include
+X11INC = /opt/X11/include
+#X11LIB = /usr/X11R6/lib
+X11LIB = /opt/X11/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
@@ -16,9 +18,9 @@ XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/include/freetype2
+#FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
-#FREETYPEINC = ${X11INC}/freetype2
+FREETYPEINC = ${X11INC}/freetype2
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
